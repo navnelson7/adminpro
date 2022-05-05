fa6be10a4a6ad36f9946d4aae44a548ae1d6f107
navnelson7 <navnelson7@gmail.com>
2022-05-05 10:57:21 -0600

    creating routing child and import RoutingModule in approuting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,12 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
 import { NopagefoundComponent } from './nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages.component';
+import { PagesRoutingModule } from './pages/pages.routing';
 
 const routes: Routes =[
-  {path: '', 
-    component: PagesComponent,
-    children: [
-      {path: 'dashboard', component: DashboardComponent},
-      {path: 'progress', component: ProgressComponent},
-      {path: 'grafica1', component: Grafica1Component},
-      {path: '', redirectTo:'/dashboard', pathMatch: 'full'}, 
-    ]
-  } ,
+  
   
 
 
@@ -29,7 +17,10 @@ const routes: Routes =[
 ]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    PagesRoutingModule
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
